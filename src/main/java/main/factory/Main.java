56b1f50b98e9d641dcdd3e377bd67ee3440211fe
Milos <milos.vukovljak94@gmail.com>
2020-07-19 11:29:30 +0200

    creates goToTravel and FuelUp methods

--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Pocetak");
         
-        
         Car audi8 = new Car();
+        audi8.printAttributes();
+        
         audi8.setModel("Audi broj 8");
         audi8.setBuildYear(2003);
         audi8.setCcm(2000);
@@ -20,13 +21,32 @@ public class Main {
         
         fiatPunto.printAttributes();
         
-        
-        Car lada = new Car();
-        lada.setModel("Lada");
-        
+        Car lada = new Car("Lada Niva", 1990, "green", 120, 50);
         lada.printAttributes();
+
+        Car opel = new Car("Opel", 2002, "blue", 200, 1300, 7, 150000);
+        opel.printAttributes();
         
+        //prvo putovanje
+        opel.setMaxFuel(100);
+        opel.setFuel(20);
+        opel.setConsumption(10);
         
+        opel.fuelUp(29);
+        opel.fuelUp(21);
+        
+        opel.goToTravel(300);
+        opel.printAttributes();
+        
+        //drugo putovanje
+        opel.goToTravel(50);
+        opel.printAttributes();
+        
+        //trece putovanje
+        opel.fuelUp(200);
+        opel.goToTravel(500);
+        opel.printAttributes();
     }
     
+
 }
